e73037ec2dd89716b8463ac37716dd871b2bc36e
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-03-21 10:52:00 +0100

    Ajout demo 1 module 3

---
+++ module3/Demonstrations/CreerEtManipulerObjet.js
@@ -0,0 +1,32 @@
+//Créer un objet
+var sport = { nom: "squash", description: "un jeu de raquettes" }
+
+//Manipuler un objet
+console.log(sport)
+console.log(sport.nom)
+sport.nom = "Squash"
+console.log(sport["nom"])
+
+//Un objet est une structure dynamique
+sport.nombreJoueurs = 2
+console.log(sport)
+
+//Un objet peut contenir des fonctions
+var sport2 = {
+    nom: "Badminton",
+    description: "ça vole",
+    afficher: function () {
+        console.log(`${this.nom.toUpperCase()} ${this.description}`)
+    }
+}
+
+sport2.afficher();
+
+//Il est possible de créer des tableaux d'objets
+var sports = [sport, sport2, { nom: "PADEL", description: "un autre jeu de raquetes" }];
+sports.push({ nom: "TENNIS", description: "ça claque" })
+console.log(sports)
+
+//Un objet peut référencer un tableau
+sport.qualificatifs = ["Rapide", "Fatiguant", "..."]
+console.log(sport)
