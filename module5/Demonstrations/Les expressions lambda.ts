7bfa9e9ccb8b94c279e282aa55528c280b6c963f
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-03-24 10:11:18 +0100

    ajout demo 6 module 5

---
+++ module5/Demonstrations/Les expressions lambda.ts
@@ -0,0 +1,18 @@
+//Déclarer une fonction classique
+var donneMoiUnEntier = function () {
+    return Math.floor(Math.random() * 10)
+}
+
+console.log(donneMoiUnEntier())
+
+//Première syntaxe d'expression lambda
+var donneMoiUnEntierV2 = () => { return Math.floor(Math.random() * 10) }
+console.log(donneMoiUnEntierV2())
+
+//Deuxième syntaxe d'expression lambda
+var donneMoiUnEntierV3 = () => Math.floor(Math.random() * 10)
+console.log(donneMoiUnEntierV3())
+
+//Cas concret
+var tableau: Array<number> = [1, 2, 5, 6, 9]
+tableau.forEach((value, index, tab) => console.log("tableau[%i]=%i", index, value))
